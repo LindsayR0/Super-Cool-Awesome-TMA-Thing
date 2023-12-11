805ba8f94e41b135b589a1d640e8e12f03286489
LindsayR0 <ross02@allegheny.edu>
2023-12-11 10:08:11 -0500

    sourced audio files

--- sounds.py
+++ sounds.py
@@ -1,10 +1,22 @@
 button_audios = {
-    'button1': 'audios/I_OPEN_THE_DOOR.wav',
-    'button2': 'audios/HelloJon.wav',
-    'button3': 'audios/EyeSpy.wav',
-    'button4': 'audios/TestTest123.wav',
-    'button5': 'audios/StatementBegins.wav',
-    'button6': 'audios/EndRecording.wav'
+    #audio pulled from episode 1 of The Magnus Archives ("MAG001 - Anglerfish"),
+    #which can be found at https://www.youtube.com/watch?v=AdiUHYacaRI
+    'button1': 'audios/StatementBegins.wav',
+    #audio pulled from episode 1 of The Magnus Archives ("MAG001 - Anglerfish"),
+    #which can be found at https://www.youtube.com/watch?v=AdiUHYacaRI
+    'button2': 'audios/EndRecording.wav''audios/HelloJon.wav',
+    #audio pulled from episode 1 of The Magnus Archives ("MAG001 - Anglerfish"),
+    #which can be found at https://www.youtube.com/watch?v=AdiUHYacaRI
+    'button3': 'audios/TestTest123.wav',
+    #audio pulled from episode 160 of The Magnus Archives ("MAG160 - The Eye Opens"),
+    #which can be found at https://www.youtube.com/watch?v=riLljv93IqQ&t=443s
+    'button4': 'audios/HelloJon.wav',
+    #audio pulled from episode 180 of The Magnus Archives ("MAG180 - Moving On"),
+    #which can be found at https://www.youtube.com/watch?v=s9K_6B3RKBs&t=18s
+    'button5': 'audios/EyeSpy.wav',
+    #audio pulled from episode 160 of The Magnus Archives ("MAG160 - The Eye Opens"),
+    #which can be found at https://www.youtube.com/watch?v=riLljv93IqQ&t=443s
+    'button6': 'audios/I_OPEN_THE_DOOR.wav'
     }
 
 button_audio_length_sec = {
